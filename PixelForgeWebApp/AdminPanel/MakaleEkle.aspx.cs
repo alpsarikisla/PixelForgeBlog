24c3a48f652c0add8e6494b01bd9f5c7b2ea740b
alpsarikisla <50963484+alpsarikisla@users.noreply.github.com>
2024-03-04 14:17:28 +0300

    Makale Ekleme Sayfası Eklendi

---
+++ PixelForgeWebApp/AdminPanel/MakaleEkle.aspx.cs
@@ -0,0 +1,25 @@
+﻿using DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PixelForgeWebApp.AdminPanel
+{
+    public partial class MakaleEkle : System.Web.UI.Page
+    {
+        DataModel dm = new DataModel();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ddl_kategoriler.DataSource = dm.KategorileriGetir();
+            ddl_kategoriler.DataBind();
+        }
+
+        protected void lbtn_ekle_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
